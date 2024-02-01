2bb276b47f4d8dd11886ca18d8ac5f79c1603ccd
Hidaya Mustafa <hidayamustafa3@gmail.com>
2024-02-01 19:39:19 +0200

    Add files via upload

---
+++ Book.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self,Title="",Publisher="",ISBN_10="",ISBN_13="",option="",NUM_of_copies=""):
+        self.Title = Title
+        self.option = option
+        self.Publisher = Publisher
+        self.ISBN_10 = ISBN_10
+        self.ISBN_13 = ISBN_13
+        self.NUM_of_copies = NUM_of_copies
+
+    def increase_copies(self, num):
+        self.NUM_of_copies += num
+
+    def decrease_copies(self, num):
+        self.NUM_of_copies -= num
